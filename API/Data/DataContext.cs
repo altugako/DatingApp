8b080ed5533e124590e5b86b56b1c8e7a0507e6a
altuga <altugako@gmail.com>
2021-12-20 21:54:02 +0200

    End of section 2

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,20 @@
+using System;
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+
+        internal object ToList()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
